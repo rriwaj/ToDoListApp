b7a8bb65bdf2c1ace69488337d084a5af24b1f21
rriwaj <riwajrimal@gmail.com>
2015-09-21 15:34:35 -0500

    initial project skeleton

---
+++ bootstrap-sample.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NerdLuv</title>
+        <meta charset="utf-8" />
+        <?php include 'bootstrap.html'?>        
+    </head>
+    <body>     
+        <button class="btn btn-default">Sample</button>
+        <div class="row show-grid">
+            <div class="col-md-4">left col</div>
+            <div class="col-md-4">mid col</div>
+            <div class="col-md-4">right col</div>
+        </div>
+        <?php include 'validator.html'?>        
+    </body>
+</html>
