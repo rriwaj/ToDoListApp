ffac173f1eaf7e6e45a1ac66b49e0d09bfc69953
rriwaj <=>
2015-09-22 17:05:30 -0500

    inserting comment done

--- commentsrear.php
+++ commentsrear.php
@@ -2,15 +2,15 @@
 
 require './include/db-connection.php';
 
-$comment_text = "hello"; // filter_input(INPUT_POST, "post");
-$item_id = 1; //filter_input(INPUT_POST, "done");
+$comment = filter_input(INPUT_POST, "comment");
+$post_id = filter_input(INPUT_POST, "post_id");
 $created_date = date("Y-m-j G:i:s ");
 //session_start();
 $user_id = 1; //  $_SESSION('userid');
 
 $stmt = $db->prepare("INSERT INTO comments VALUES (NULL,:text, :userid, :itemid, :date)");
-$stmt->execute(array(':text' => $comment_text, ':userid' => $user_id, ':itemid' => $item_id
+$stmt->execute(array(':text' => $comment, ':userid' => $user_id, ':itemid' => $post_id
     , ':date' => $created_date));
 
-echo " Finished";
+echo "{'success':'true'}";
 
