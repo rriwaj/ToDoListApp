d8b4e5cb2e187ac5e4c2b16649cc1a1961957dcf
dheeraj <rythmnite66@gmail.com>
2015-09-24 16:27:30 -0500

    signup1.php added

---
+++ signup1.php
@@ -0,0 +1,37 @@
+<?php include 'include/top.html' ?>
+
+<div class="container">
+    <div class="container-page">	
+        <form  action="register.php" method="post">
+
+            <div class="col-md-6">
+                <h3 class="dark-grey">Registration</h3>
+
+                <div class="form-group col-lg-12">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control" id="" >
+                </div>
+
+                <div class="form-group col-lg-6">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" id="">
+                </div>
+
+                <div class="form-group col-lg-6">
+                    <label>Confirm Password</label>
+                    <input type="password" name="" class="form-control" id="" value="">
+                </div>
+
+                <div class="form-group col-lg-12">
+                    <label>Email Address</label>
+                    <input type="email" name="emailAddress" class="form-control" id="" >
+                </div>
+
+                <input  class="btn btn-lg btn-primary btn-block" type="submit"  value="Register"/>
+            </div>
+        </form>
+
+    </div>
+
+</div>
+<?php include 'include/bottom.html' ?>
